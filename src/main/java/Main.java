b6194742a5c4237c5a54f6e5a429f27904a58ebe
BarbaraPieri <barbara.pieriv@gmail.com>
2024-02-10 11:51:03 -0300

    Fazendo consulta para API com uri dinâmica.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,14 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Realizar a consulta à API externa
-        String respostaAPI = APIRequest.consultarAPIExterna();
-
-        // Imprimir o resultado da consulta
-        System.out.println("Resposta da API:");
-        System.out.println(respostaAPI);
-
-        // situação anterior - Criando um objeto Scanner para ler a entrada do teclado
+        //Criando um objeto Scanner para ler a entrada do teclado
         Scanner scanner = new Scanner(System.in);
 
         try {
@@ -21,32 +14,18 @@ public class Main {
             // Lendo a quantidade digitada pelo usuário
             int quantidade = scanner.nextInt();
 
-            // Criando um objeto Livro com os dados fornecidos pelo usuário
-            Livro livro = new Livro("Nome do Livro", "Descrição do Livro", 0.0, quantidade, "ISBN do Livro", 0);
+            // Construindo a URI dinâmica com base na quantidade fornecida
+            String uri = "https://fakestoreapi.com/products/" + quantidade;
 
-            // Imprimindo os dados do livro
-            livro.imprimirDados();
+            // Realizando a consulta à API externa
+            String respostaAPI = APIRequest.consultarAPIExterna();
 
-            // Criando um objeto Eletronico com os dados fornecidos pelo usuário
-            System.out.println("\nAgora, vamos criar um produto eletrônico:");
-
-            // Pedindo para o usuário digitar a voltagem do eletrônico
-            System.out.print("Digite a voltagem do eletrônico: ");
-            int voltagem = scanner.nextInt();
-
-            // Pedindo para o usuário digitar a potência do eletrônico
-            System.out.print("Digite a potência do eletrônico: ");
-            int potencia = scanner.nextInt();
-
-            // Criando um objeto Eletronico com os dados fornecidos pelo usuário
-            Eletronico eletronico = new Eletronico("Nome do Eletrônico", "Descrição do Eletrônico", 0.0, quantidade, voltagem, potencia);
-
-            // Imprimindo os dados do eletrônico
-            eletronico.imprimirDados();
+            // Imprimindo o resultado da consulta
+            System.out.println("Resposta da API:");
+            System.out.println(respostaAPI);
         } catch (InputMismatchException e) {
-            System.out.println("Erro: Por favor, insira um valor numérico válido.");
+            System.out.println("Erro: Por favor, insira um valor numérico válido para a quantidade do produto.");
         } finally {
-
             // Fechando o objeto Scanner
             scanner.close();
         }
