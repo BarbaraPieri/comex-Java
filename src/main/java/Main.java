92f4bae0a1b488a5b7c039c14ec3adbfda6da299
BarbaraPieri <barbara.pieriv@gmail.com>
2024-02-10 11:26:03 -0300

    Tratando uma possível exceção ao digitar texto na leitura de dados.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -5,36 +6,42 @@ public class Main {
         // Criando um objeto Scanner para ler a entrada do teclado
         Scanner scanner = new Scanner(System.in);
 
-        // Pedindo para o usuário digitar a quantidade do produto
-        System.out.print("Digite a quantidade do produto: ");
+        try {
 
-        // Lendo a quantidade digitada pelo usuário
-        int quantidade = scanner.nextInt();
+            // Pedindo para o usuário digitar a quantidade do produto
+            System.out.print("Digite a quantidade do produto: ");
 
-        // Criando um objeto Livro com os dados fornecidos pelo usuário
-        Livro livro = new Livro("Nome do Livro", "Descrição do Livro", 0.0, quantidade, "ISBN do Livro", 0);
+            // Lendo a quantidade digitada pelo usuário
+            int quantidade = scanner.nextInt();
 
-        // Imprimindo os dados do livro
-        livro.imprimirDados();
+            // Criando um objeto Livro com os dados fornecidos pelo usuário
+            Livro livro = new Livro("Nome do Livro", "Descrição do Livro", 0.0, quantidade, "ISBN do Livro", 0);
 
-        // Criando um objeto Eletronico com os dados fornecidos pelo usuário
-        System.out.println("\nAgora, vamos criar um produto eletrônico:");
+            // Imprimindo os dados do livro
+            livro.imprimirDados();
 
-        // Pedindo para o usuário digitar a voltagem do eletrônico
-        System.out.print("Digite a voltagem do eletrônico: ");
-        int voltagem = scanner.nextInt();
+            // Criando um objeto Eletronico com os dados fornecidos pelo usuário
+            System.out.println("\nAgora, vamos criar um produto eletrônico:");
 
-        // Pedindo para o usuário digitar a potência do eletrônico
-        System.out.print("Digite a potência do eletrônico: ");
-        int potencia = scanner.nextInt();
+            // Pedindo para o usuário digitar a voltagem do eletrônico
+            System.out.print("Digite a voltagem do eletrônico: ");
+            int voltagem = scanner.nextInt();
 
-        // Criando um objeto Eletronico com os dados fornecidos pelo usuário
-        Eletronico eletronico = new Eletronico("Nome do Eletrônico", "Descrição do Eletrônico", 0.0, quantidade, voltagem, potencia);
+            // Pedindo para o usuário digitar a potência do eletrônico
+            System.out.print("Digite a potência do eletrônico: ");
+            int potencia = scanner.nextInt();
 
-        // Imprimindo os dados do eletrônico
-        eletronico.imprimirDados();
+            // Criando um objeto Eletronico com os dados fornecidos pelo usuário
+            Eletronico eletronico = new Eletronico("Nome do Eletrônico", "Descrição do Eletrônico", 0.0, quantidade, voltagem, potencia);
 
-        // Fechando o objeto Scanner
-        scanner.close();
+            // Imprimindo os dados do eletrônico
+            eletronico.imprimirDados();
+        } catch (InputMismatchException e) {
+            System.out.println("Erro: Por favor, insira um valor numérico válido.");
+        } finally {
+
+            // Fechando o objeto Scanner
+            scanner.close();
+        }
     }
 }
